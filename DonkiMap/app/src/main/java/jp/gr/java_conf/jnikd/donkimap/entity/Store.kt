2a8dc164a3610b05b4e250d6ca2f28283c9f6a8f
jnikd <jnikd0826@gmail.com>
2017-09-22 19:03:20 +0900

    move adapter code.

--- DonkiMap/app/src/main/java/jp/gr/java_conf/jnikd/donkimap/entity/Store.kt
+++ DonkiMap/app/src/main/java/jp/gr/java_conf/jnikd/donkimap/entity/Store.kt
@@ -1,8 +1,19 @@
 package jp.gr.java_conf.jnikd.donkimap.entity
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+
 data class StoreList(
         val list: List<Store>
-)
+) {
+    companion object {
+        fun adapter(): JsonAdapter<StoreList> {
+            return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+                    .adapter(StoreList::class.java)
+        }
+    }
+}
 
 data class Store(
         val name: String,
